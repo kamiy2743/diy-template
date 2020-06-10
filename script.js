5e1814f8b9f5541cfa02be5a425d2a63989a3b34
kamiy2743 <46000642+kamiy2743@users.noreply.github.com>
2020-06-10 23:19:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
 $(function() {
-
-  $(".get-if-btn").click(function() {
-    var $parent = $(this).parents(".item");
-    if ($(this).hasClass("get-selected")) {
-      $(this).removeClass("get-selected").text("未取得");
-      $parent.find(".dist-if-btn").removeClass("dist-selected").text("不可");
-      $parent.find("select").prop("disabled",true);
-      $parent.find("option").prop("selected",false);
-    } else {
-      $(this).addClass("get-selected").text("取得済");
-    }
-  });
-  $(".dist-if-btn").click(function() {
-    var $parent = $(this).parents(".item");
-    if ($(this).hasClass("dist-selected")) {
-      $(this).removeClass("dist-selected").text("不可");
-      $parent.find("select").prop("disabled",true);
-      $parent.find("option").prop("selected",false);
-    } else {
-      $(this).addClass("dist-selected").text("可");
-      $parent.find(".get-if-btn").addClass("get-selected").text("取得済");
-      $parent.find("select").prop("disabled",false);
-      $parent.find(".opsele").prop("selected",true);
-    }
-  });
-
+  
   $("#list-search select").change(function() {
     var cate = $("#category-list").val();
     var seri = $("#series-list").val();
