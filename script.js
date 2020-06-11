3f470000fce9f8232b331870132ed7e288795ff7
kamiy2743 <46000642+kamiy2743@users.noreply.github.com>
2020-06-11 11:48:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,31 +15,6 @@ $(function() {
     });
   });
 
-  $(".get-if-btn").on("click touchstart",function() {
-    var $parent = $(this).parents(".item");
-    if ($(this).hasClass("get-selected")) {
-      $(this).removeClass("get-selected").text("未取得");
-      $parent.find(".dist-if-btn").removeClass("dist-selected").text("不可");
-      $parent.find("select").prop("disabled",true);
-      $parent.find("option").prop("selected",false);
-    } else {
-      $(this).addClass("get-selected").text("取得済");
-    }
-  });
-  $(".dist-if-btn").click(function() {
-    var $parent = $(this).parents(".item");
-    if ($(this).hasClass("dist-selected")) {
-      $(this).removeClass("dist-selected").text("不可");
-      $parent.find("select").prop("disabled",true);
-      $parent.find("option").prop("selected",false);
-    } else {
-      $(this).addClass("dist-selected").text("可");
-      $parent.find(".get-if-btn").addClass("get-selected").text("取得済");
-      $parent.find("select").prop("disabled",false);
-      $parent.find(".opsele").prop("selected",true);
-    }
-  });
-
   $(".fa-times").click(function() {
     $("#detail-container").hide();
   });
