2b116b852d56a640b93730b0a0c2f22ae0f35c07
kamiy2743 <46000642+kamiy2743@users.noreply.github.com>
2020-06-11 18:27:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ $(function() {
   $(".fa-arrows-alt-h").click(function() {
     $("#detail-container").toggleClass("small");
     if ($("#detail-container").hasClass("small")) {
-      $("#detail-container").css("top","80px");
+      $("#detail-container").css("top","90px");
     } else {
-      $("#detail-container").css("top","105px");
+      $("#detail-container").css("top","115px");
     }
   });
 
